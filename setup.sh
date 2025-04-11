e0bdc00218b3b7b670360f44106876f44097d3f3
clement <clement.boyer42360@gmail.com>
2025-04-11 10:26:11 +0200

    fix: directory name before installation

--- setup.sh
+++ setup.sh
@@ -203,7 +203,7 @@ git clone --depth 1 https://github.com/Clementb42/hyprland.git
 echo ":: Installation files cloned into Downloads folder"
 
 # Change into the folder
-cd hyprland-starter
+cd hyprland
 
 # Start the script
 ./install.sh
